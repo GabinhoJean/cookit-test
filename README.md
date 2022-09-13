ac6a9efaf355b32ba28c9032c4d2b574168b3dd2
Gabinho <jgabin200@gmail.com>
2022-09-13 15:51:58 -0400

    init of repo

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# cookit-test
+# Cook it - test
+
+## Consignes
+
+- Planifier la mise en place d'un kafka via AWS, pousser du contenu sur un topic et le consommer.
+- Documenter où tu es rendu, ce qui t'a bloqué éventuellement et quel serait ton plan pour le finaliser.
+
+## Approche et Planification
+
+AWS propose un service Apache Kafka entièrement géré et hautement disponible du nom de [AWS MSK](https://aws.amazon.com/msk/).
+
+Le gros de ce travail se résume donc à l'automatisation du déploiment MSK via Terraform.
+
+j'ai opté pour Terraform pour bénéficier de tous les avantages de l'approche IaC.
+
+Il existe un module open source pour terraform permettant de déployer un cluster MSK sur AWS. Ce module est disponible ici [terraform-aws-msk-kafka-cluster](https://github.com/clowdhaus/terraform-aws-msk-kafka-cluster/tree/v1.2.0).
+
+Ce travail propose un [exemple introductif](https://github.com/clowdhaus/terraform-aws-msk-kafka-cluster/tree/v1.2.0/examples/basic) sur lequelle je me suis basé.
+
+## Niveau de progression
+
+A ce stade j'ai préparé les fichiers nécéssaires mais aucun test n'a été fait sur un compte AWS.
+
+Pour tester il faudrait : 
+
+1 - editer le fichier main.tf et y définir les valeurs suivantes du pour se connecter au compte AWS :
+
+```bash
+provider "aws" {
+    ...
+shared_config_files      = ["/Users/tf_user/.aws/conf"]
+shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+...
+}
+```
+Ou (approche non recommandé dans la pratique)
+
+```bash
+provider "aws" {
+    ...
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+...
+}
+
+2 - Puis, exécuter les commandes suivantes :
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+## Perpectives pour la suite
+
+Pour la suite il faudrait addresser les points suivants :
+
+- un véritable dimessionnement du cluster kafka (nombres de brokers, nombre de partitions et replication par topic)
+
+- Définir la politique de sécurisation  et d'accès au cluster Kafka. Ici plusieurs pistes sont envisagles: les controles d'accès aux VPC, des comptes IAM avec des roles spécifiques; l'utilisation de kafka security pour gérer les connexion autorisées sur le cluster et bien d'autres pistes. Une bonne documentation sur le sujet proposé par AWS est [disponible ici](https://docs.aws.amazon.com/msk/latest/developerguide/security.html).
+
+- la creation d'un Topic de test et l'utilisation de producers et consumers sur ce topic.
+
+- Opter pour un stockage distant et partagé pour le state de Terraform.
+
---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = local.region
+#  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+#  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+#  access_key = "my-access-key"
+#  secret_key = "my-secret-key"
+}
+
+locals {
+  region = "us-east-1"
+  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
+
+  tags = {
+    Example     = local.name
+    Environment = "dev"
+  }
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.0.0.0/18"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+
+  create_database_subnet_group = true
+  enable_nat_gateway           = true
+  single_nat_gateway           = true
+  map_public_ip_on_launch      = false
+
+  manage_default_security_group  = true
+  default_security_group_ingress = []
+  default_security_group_egress  = []
+
+  enable_flow_log                      = true
+  flow_log_destination_type            = "cloud-watch-logs"
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+  flow_log_log_format                  = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${vpc-id} $${subnet-id} $${instance-id} $${tcp-flags} $${type} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${az-id} $${sublocation-type} $${sublocation-id}"
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Security group for ${local.name}"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  ingress_rules       = ["kafka-broker-tcp", "kafka-broker-tls-tcp"]
+}
+
+################################################################################
+# MSK Cluster - Disabled
+################################################################################
+
+module "msk_cluster_disabled" {
+  source = "clowdhaus/msk-kafka-cluster/aws"
+
+  create = false
+}
+
+################################################################################
+# MSK Cluster - Default
+################################################################################
+
+module "msk_cluster" {
+  source = "clowdhaus/msk-kafka-cluster/aws"
+
+  name                   = local.name
+  kafka_version          = "2.8.0"
+  number_of_broker_nodes = 3
+
+  broker_node_client_subnets  = module.vpc.private_subnets
+  broker_node_ebs_volume_size = 20
+  broker_node_instance_type   = "kafka.t3.small"
+  broker_node_security_groups = [module.security_group.security_group_id]
+
+  tags = local.tags
+}
